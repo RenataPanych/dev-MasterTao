769865e3666c9befdaf68fcbbd86105a065e41ae
RenataPanych <renatapanych222@gmail.com>
2024-04-08 09:58:22 +0300

    main

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,4 +1,11 @@
-$('select').niceSelect();
+const hamburger = document.querySelector('.hamburger')
+const headerNav = document.querySelector('.header-nav')
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('is-active')
+    headerNav.classList.toggle('show')
+})
+
 
 $('#first-screen-slider').slick({
     dots: true,
@@ -12,87 +19,24 @@ $('#first-screen-slider').slick({
 });
 
 
-$('.slider-template').slick({
-  infinite: true,
-  slidesToShow: 3,
-  responsive: [{
-    breakpoint: 1400,
-    settings: {
-      slidesToShow: 2,
-    }
-}, 
-{
-  breakpoint: 1025,
-  settings: {
-    slidesToShow: 1,
-    arrows: false,
-    dots: true,
-  }
-}, 
-]
-});
+$('select').niceSelect();
 
-
-
-function mobileOnlySlider($slidername, $breakpoint) {
-  const slider = $($slidername);
-  const settings = {
-    mobileFirst: true,
-    dots: true,
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: $breakpoint,
-        settings: "unslick"
-      }
-    ]
-  };
-  slider.slick(settings);
-
-  $(window).on("resize", function () {
-    if ($(window).width() > $breakpoint) {
-      return;
-    }
-    if (!slider.hasClass("slick-initialized")) {
-      return slider.slick(settings);
-    }
-  });
-} // Mobile Only Slider
-
-mobileOnlySlider(".slider-advantages", 1024);
-
-
-
-const hamburger = document.querySelector('.hamburger')
-const headerNav = document.querySelector('.header-nav')
 const fileInput = document.querySelector('#calculation-file');
 const fileName = document.querySelector('#file-name')
-const controledField = document.querySelectorAll('[type="checkbox"]')
-const fileField = document.querySelectorAll('[type="file"]')
-const formBtns = document.querySelectorAll('form .col button')
-const body = document.querySelector('body')
-
-hamburger.addEventListener('click', function() {
-    hamburger.classList.toggle('is-active')
-    headerNav.classList.toggle('show')
-    body.classList.toggle('overflow-hidden')
-})
-
 
 fileInput.addEventListener('change', () => {
     fileName.innerHTML = fileInput.files[0].name;
 });
 
 
-const addParentClass = (array, parent, className)=> {
-    array.forEach(item => {
-        item.closest(parent).classList.add(className)
-    })
-}
+const controledField = document.querySelectorAll('[type="checkbox"]')
+const fileField = document.querySelectorAll('[type="file"]')
 
-addParentClass(formBtns, '.col', 'mobile-center')
-addParentClass(controledField, '.input-wrapper', 'controled-wrapper')
-addParentClass(controledField, '.col', 'controled-col')
-addParentClass(fileField, '.input-wrapper', 'file-wrapper')
+controledField.forEach(input => {
+    input.closest('.input-wrapper').classList.add('controled-wrapper')
+    input.closest('.col').classList.add('controled-col')
+})
 
-
+fileField.forEach(input => {
+  input.closest('.input-wrapper').classList.add('file-wrapper')
+})
